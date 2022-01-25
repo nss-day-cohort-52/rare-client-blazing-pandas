d57d8d4dc930d3e4f96292e15442af7d8dfed2d7
Sean Derevjanik <sean.derevjanik@gmail.com>
2022-01-25 13:06:07 -0700

    client side implementation of tags list. buttons and submissions are currently nonfunctional

---
+++ src/components/tags/TagCreation.js
@@ -0,0 +1,15 @@
+import react, {useRef} from "react"
+
+export const CreateTag = () => {
+    const newTag = useRef()
+
+    return <section>
+        <h3>Create a new Tag</h3>
+        <div className="field">
+          <label className="label">Label</label>
+          <div className="control">
+            <input className="input" type="text" ref={newTag} />
+          </div>
+        </div>
+    </section>
+}
