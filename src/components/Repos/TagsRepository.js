964bc1ddfd8e94bed4f88188ff517a4acfb799a4
Sean Derevjanik <sean.derevjanik@gmail.com>
2022-01-25 14:53:46 -0700

    added functionality to create tags button

--- src/components/Repos/TagsRepository.js
+++ src/components/Repos/TagsRepository.js
@@ -2,3 +2,13 @@ export const getTags = () => {
     return fetch("http://localhost:8088/tags")
         .then(res => res.json())
 }
+
+export const addTag = tag => {
+    return fetch("http://localhost:8088/tags", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(tag)
+    }).then(getTags)
+}
--- src/components/tags/TagCreation.js
+++ src/components/tags/TagCreation.js
@@ -1,7 +1,13 @@
 import react, {useRef} from "react"
+import { addTag } from "../Repos/TagsRepository"
 
-export const CreateTag = () => {
+export const CreateTag = ({sync}) => {
     const newTag = useRef()
+    const syncTags = sync
+    const CreateTag = () => {
+        const fullTag = {"label":newTag.current.value}
+        addTag(fullTag).then(sync)
+    }
 
     return <section>
         <h3>Create a new Tag</h3>
@@ -10,7 +16,7 @@ export const CreateTag = () => {
           <div className="control">
             <input className="input" type="text" ref={newTag} />
           </div>
-          <button className="button is-link" type="submit" >Submit</button>
+          <button className="button is-link" type="submit" onClick={() => CreateTag()}>Submit</button>
         </div>
     </section>
 }
