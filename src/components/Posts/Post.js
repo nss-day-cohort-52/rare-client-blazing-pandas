ffc1f43ae091137dd2808231da0860cbb7286031
keosha-w <keosha.watkins@asurion.com>
2022-01-26 09:20:18 -0600

    added some basic css.

--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -1,25 +1,35 @@
 import { useState } from "react"
-export default ({post}) => {
-    
+import "./Post.css"
+export default ({ post }) => {
+
     const [details, setdetails] = useState(false)
 
 
     return (
         <>
-            { details
+            {details
                 ? <div>
-                        <p>{post?.title}</p>
-                        <p>{post.user.username}</p>
-                        <p>Category</p>
-                        <p>Publication date</p>
-                        <p>Content</p>
+                    <p>{post?.title}</p>
+                    <p>{post.user.username}</p>
+                    <p>Category</p>
+                    <p>Publication date</p>
+                    <p>Content</p>
                 </div>
-                : <div>
-                        <h3>{post.title}</h3> <img src={post.image_url} /> <p>{post.user.username}</p> <p>{post.category.label}</p>
+                : <div class="post">
+                    <div>
+                        <h2>{post?.title}</h2>
+                    </div>
+                    <div><img src={post.image_url} /></div> 
+                    <div>
+                        <p>Author: {post.user.username}</p>
+                    </div> 
+                    <div>
+                        <p>{post.category.label}</p>
+                    </div>
                 </div>
 
             }
         </>
     )
-    
+
 }
