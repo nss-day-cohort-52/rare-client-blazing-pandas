f8f5ec735ecde39bf7cfcc118330557d79ed97c1
auntviv <aviva.mondesir@gmail.com>
2022-01-27 14:53:40 -0600

    Category Functionality

--- src/components/Repos/CategoriesRepository.js
+++ src/components/Repos/CategoriesRepository.js
@@ -1,20 +1,3 @@
-// import { useEffect, useState } from "react"
-
-// export const getCategories = () => {
-// const [categories, setCategories] = useState([])
-//     useEffect(
-//         () => {
-//             fetch("http://localhost:8088/categories")
-//                 .then(res => res.json())
-//                 .then((categories) => {
-//                     setCategories(categories)
-//                 })
-
-//         },[]
-//     )
-// }
-
-
 export const getCategories = () => {
     return fetch("http://localhost:8088/categories")
         .then(res => res.json())
