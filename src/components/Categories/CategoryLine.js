b13483ed0c49a52e7fcee947801a39bb02466357
Sean Derevjanik <sean.derevjanik@gmail.com>
2022-02-15 19:34:49 -0700

    modified old delete, added edit functionality for categories

--- src/components/Categories/CategoryLine.js
+++ src/components/Categories/CategoryLine.js
@@ -1,9 +1,35 @@
-export const CategoryLine = ({label}) => {
+import { useState } from "react"
+import { deleteCategory, editCategory } from "../Repos/CategoriesRepository"
+
+export const CategoryLine = ({ cat, sync }) => {
+    const [editing, setEditing] = useState(false)
+    const [cate, setCate] = useState(cat)
+    const updateCate = (e) => {
+        const copy = cate
+        copy[e.target.name] = e.target.value //could do with a useRef in theory
+        setCate(copy)
+    }
+    const flipEdit = () => setEditing(!editing)
+
     return <>
         <div>
-            <text>{label}</text>
-            <button className="button is-link" type="submit" >edit</button>
-            <button className="button is-link is-light" type="submit" >delete</button>
+            {editing ?
+                <div className="field">
+                    <label className="label">New Label: (previously {`${cat.label}`})</label>
+                    <div className="control">
+                        <input className="input" type="text" name="label" onChange={updateCate}/>
+                    </div>
+                    <button className="button is-link" type="submit" onClick={() => {
+                        editCategory(cate).then(sync)
+                        flipEdit()
+                    }
+                    }>Save</button>
+                </div> : <>
+                    <text>{cat.label}</text>
+                    <button className="button is-link" type="submit" onClick={flipEdit}>edit</button>
+                </>
+            }
+            <button className="button is-link is-light" type="submit" onClick={() => deleteCategory(cat.id).then(sync)}>delete</button>
         </div>
     </>
 }
